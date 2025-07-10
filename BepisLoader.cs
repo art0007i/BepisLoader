c8f36c5f1e282eeb7c559c3c4f53597192eae8ae
art0007i <art0007i@gmail.com>
2025-07-10 23:18:29 +0200

    use an AssemblyLoadContext

--- BepisLoader.cs
+++ BepisLoader.cs
@@ -1,70 +1,116 @@
-﻿using System.Diagnostics;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Loader;
 
 namespace BepisLoader;
 
 public class BepisLoader
 {
     internal static string resoDir = string.Empty;
+    internal static AssemblyLoadContext alc = null!;
     static void Main(string[] args)
     {
 #if DEBUG
         File.WriteAllText("Assemblies.log", "BepisLoader started\n");
 #endif
-        var currentPath = Process.GetCurrentProcess().MainModule!.FileName;
-        resoDir = Path.GetDirectoryName(currentPath)!;
-        var resoDllPath = Path.Combine(resoDir, "Resonite.dll");
+        resoDir = Directory.GetCurrentDirectory();
 
-        Log($"Loading: {resoDllPath}");
+        alc = new BepisLoadContext();
 
+        // TODO: removing this breaks stuff, idk why
         AppDomain.CurrentDomain.AssemblyResolve += ResolveGameDll;
 
-        StartupHook.Initialize(resoDllPath);
+        var asm = alc.LoadFromAssemblyPath(Path.Combine(resoDir, "BepInEx", "core", "BepInEx.NET.CoreCLR.dll"));
 
-        LoadResonite.Load(args);
+        var resoDllPath = Path.Combine(resoDir, "Resonite.dll");
+
+        var t = asm.GetType("StartupHook");
+        var m = t.GetMethod("Initialize", BindingFlags.Public | BindingFlags.Static, [typeof(string), typeof(AssemblyLoadContext)]);
+        m.Invoke(null, [resoDllPath, alc]);
+
+        // Find and load Resonite
+        var resoAsm = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.GetName().Name == "Resonite");
+        if (resoAsm == null)
+        {
+            resoAsm = alc.LoadFromAssemblyPath(Path.Combine(resoDir, "Resonite.dll"));
+        }
+        try
+        {
+            var result = resoAsm.EntryPoint!.Invoke(null, [args]);
+            if (result is Task task) task.Wait();
+        }
+        catch (Exception e)
+        {
+            File.WriteAllLines("BepisCrash.log", [DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + " - Resonite crashed", e.ToString()]);
+        }
     }
 
-    internal static Dictionary<string, Assembly> loadedAssemblies = new();
-
-    static Assembly? ResolveGameDll(object? obj, ResolveEventArgs args)
+    static Assembly? ResolveGameDll(object? sender, ResolveEventArgs args)
     {
-        var assemblyName = new AssemblyName(args.Name).Name;
-        Log($"Resolving assembly: {args.Name}");
+        var assemblyName = new AssemblyName(args.Name);
 
-        if (assemblyName == null)
-        {
-            Log($"what?: {args.Name}");
-            return null;
-        }
-
-        var found = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.GetName().Name == assemblyName);
+        var found = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.GetName().Name == assemblyName.Name);
         if (found != null)
         {
-            Log($"(already loaded): {args.Name}");
-            loadedAssemblies[assemblyName] = found;
             return found;
         }
 
-        if (loadedAssemblies.TryGetValue(assemblyName, out var asm)) {
-            Log($"(got from cache): {args.Name}");
+        if (assemblyName.Name == "System.Management") return null;
+
+        var targetPath = Path.Combine(resoDir, assemblyName.Name + ".dll");
+        if (File.Exists(targetPath))
+        {
+            var asm = alc.LoadFromAssemblyPath(targetPath);
             return asm;
         }
 
-        var targetPath = Path.Combine(resoDir, assemblyName + ".dll");
-        if (File.Exists(targetPath))
-        {
-            var asmBytes = File.ReadAllBytes(targetPath);
-            asm = Assembly.Load(asmBytes);
-            loadedAssemblies[assemblyName] = asm;
-            Log($"Loaded assembly: {assemblyName} from {targetPath}");
-            return asm;
-        }
-        //if (File.Exists(Paths.ExecutablePath) && assemblyName.Name == "Resonite")
-        //{
-        //    File.ReadAllBytes();
-        //    return Assembly.LoadFrom(Paths.ExecutablePath);
-        //}
         return null;
+    }
+
+    private class BepisLoadContext : AssemblyLoadContext
+    {
+        protected override Assembly? Load(AssemblyName assemblyName)
+        {
+            var found = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.GetName().Name == assemblyName.Name);
+            if (found != null)
+            {
+                return found;
+            }
+
+            if (assemblyName.Name == "System.Management") return null;
+
+            var targetPath = Path.Combine(resoDir, assemblyName.Name + ".dll");
+            if (File.Exists(targetPath))
+            {
+                var asm = LoadFromAssemblyPath(targetPath);
+                return asm;
+            }
+            return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            var rid = RuntimeInformation.RuntimeIdentifier;
+            var nativeLibs = Path.Join(resoDir, "runtimes", rid, "native");
+            var potential = Path.Combine(nativeLibs, GetUnmanagedLibraryName(unmanagedDllName));
+            if (File.Exists(potential))
+            {
+                return LoadUnmanagedDllFromPath(potential);
+            }
+            return IntPtr.Zero;
+        }
+
+        private static string GetUnmanagedLibraryName(string name)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                return $"{name}.dll";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                return $"lib{name}.so";
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                return $"lib{name}.dylib";
+
+            throw new PlatformNotSupportedException();
+        }
     }
 
 #if DEBUG
